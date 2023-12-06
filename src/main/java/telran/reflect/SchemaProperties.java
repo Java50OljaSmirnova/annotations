53744ec9596692bd54347b3cecc3b2378f01c341
oljas <olja.smirnov93@gmail.com>
2023-12-06 21:54:18 +0200

    HW#50 adds annotations BeforeEach, Id, Index and appropriate processing

---
+++ src/main/java/telran/reflect/SchemaProperties.java
@@ -0,0 +1,29 @@
+package telran.reflect;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+public class SchemaProperties {
+	public static void displayFieldProperties(Object obj) throws Exception{
+		Field idField = null;
+		List<Field> indexFields = new ArrayList<>();
+		for(Field field : obj.getClass().getDeclaredFields()) {
+			if (field.isAnnotationPresent(Id.class)) {
+				if (idField != null) {
+					throw new IllegalStateException("Field Id must be one");
+				}
+				idField = field;
+			} else if(field.isAnnotationPresent(Index.class)) {
+				indexFields.add(field);
+			}
+		}
+		if(idField == null) {
+			throw new IllegalStateException("No field Id found");
+		}
+		System.out.println("id field is " + idField.getName());
+		System.out.println("index fields follow:");
+		indexFields.forEach(field -> System.out.print(field.getName() + " "));
+		System.out.println("\n");
+	}
+
+}
--- src/main/java/telran/test/TestLibrary.java
+++ src/main/java/telran/test/TestLibrary.java
@@ -1,18 +1,39 @@
 package telran.test;
 
 import java.lang.reflect.Method;
+import java.util.*;
 
+import telran.reflect.BeforeEach;
 import telran.reflect.Test;
 
 public class TestLibrary {
 	public static void launchTest(Object testObj) throws Exception {
 		Method[] methods = testObj.getClass().getDeclaredMethods();
+		List<Method> beforeEachMethods = getBeforeEachMethods(methods);
 		for(Method method : methods) {
 			if(method.isAnnotationPresent(Test.class)) {
+				beforeEachMethods.forEach(m -> {
+					try {
+						m.invoke(testObj);
+					} catch (Exception e) {
+						throw new RuntimeException(e);
+					}
+				});
 				method.setAccessible(true);
 				method.invoke(testObj);
 			}
 		}
+	}
+
+	private static List<Method> getBeforeEachMethods(Method[] methods) {
+		List<Method> res = new ArrayList<>();
+		for(Method method : methods) {
+			if (method.isAnnotationPresent(BeforeEach.class)) {
+				method.setAccessible(true);
+				res.add(method);
+			}
+		}
+		return res;
 	}
 
 }
